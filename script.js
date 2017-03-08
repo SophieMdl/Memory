2a2c510f083d8099c72bbab9d094727fbf9ea459
sophie_mdl <sophie.michaudel@gmail.com>
2017-03-08 15:54:22 +0100

    Memory version correcte

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 (function(){
 	var score = 0;
-	var champScore = document.querySelector('.score span');
-	champScore.innerHTML = score;
+	var nbClick = 0;
+	var scoreField = document.querySelector('.score span');
+	var clickField = document.querySelector('.coup span');
 
-	var myImg = new Image();
+	var pokeball = new Image();
+		pokeball.src = 'images/pokeball.svg';
 
-		myImg.src = 'images/apple.jpg';
-	
-	var cards = document.querySelectorAll(".card");
-	var front = document.querySelectorAll(".front");
-	var cardsLength = cards.length;
+
+	var plateau = document.querySelector("#plateau")
+	var flippedCards = [];
+	var card, front, bg;
+	var j, x, i;
+/*	var cardsLength = cards.length;*/
+
 	var bgCards = [
-		"images/apple.jpg",
-		"images/flower.jpg",
-		"images/moon.jpg",
-		"images/flower.jpg",
-		"images/moon.jpg",
-		"images/apple.jpg"
+		"images/charmander.svg",
+		"images/pikachu.svg",
+		"images/squirtle.svg",
+		"images/snorlax.svg",
+		"images/charmander.svg",
+		"images/pikachu.svg",
+		"images/squirtle.svg",
+		"images/snorlax.svg",
+		"images/eevee.svg",
+		"images/eevee.svg",
+		"images/meowth.svg",
+		"images/meowth.svg"
 	]
-	var flippedCards = []
+	var bgCardsLength = bgCards.length;
+	
+	shuffle(bgCards);
+
+	//Création de chaque carte avec un front particulier et ajout d'un écouteur au clic
+	for(var i = 0 ; i < bgCardsLength ; i++){
+		card = document.createElement("div");
+		card.setAttribute("class","card");
+		card.innerHTML = "<figure class='back'></figure><figure class='front'></figure>";
+		plateau.appendChild(card);
+		front = card.childNodes[1];
+	 	bg = "url(" + bgCards[i] + ") no-repeat";
+	 	front.style.background = bg;
+		card.addEventListener('click', function(){
+	 		addFlipped(this);
+	 		nbClick++;
+			clickField.innerHTML = nbClick;
+	 	});
+	 	
+	}
 
 	function shuffle(tab) {
-    	var j, x, i;
 	    for (i = tab.length; i > 0; i--) {
 	        j = Math.floor(Math.random() * i);
 	        x = tab[i - 1];
@@ -29,37 +57,34 @@
 	        tab[j] = x;
 	    }
 	}
-	shuffle(bgCards);
 
-	function checkResult(){
-	if(flippedCards[0].innerHTML == flippedCards[1].innerHTML){
-			score++;
-			champScore.innerHTML = score;
-			flippedCards = []
-		}
-	}
 	function addFlipped(target){
 		if(!target.classList.contains('flipped')){
 			target.classList.add('flipped');
 			flippedCards.push(target);
-			removeFlipped();
+			if(flippedCards.length > 1){
+				removeFlipped();
+			}
 		}
 	}
+
 	function removeFlipped(){
 			if (flippedCards.length > 2){
 			flippedCards[0].classList.remove('flipped');
-			console.log(flippedCards[0])
 			flippedCards.shift();
-			setTimeout(checkResult, 500);
 		}
+		checkResult();
 	}
-	for (var i = 0 ; i < cardsLength ; i++){
-		var bg = "url(" + bgCards[i] + ")";
-		front[i].style.background = bg;
-		cards[i].addEventListener('click', function(){
-			addFlipped(this);
-		});
-	}
-
+	
+	function checkResult(){
+		if(flippedCards[0].innerHTML == flippedCards[1].innerHTML){
+				score++;
+				scoreField.innerHTML = score;
+				flippedCards = []
+				if(score == 6){
+					alert("Amazing !!");
+				}
+			}
+		}
 }());
-					
+				
